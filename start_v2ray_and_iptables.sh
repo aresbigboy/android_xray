1b574078cb396972eac76825726052665997c4da
aresbigboy <wei.ares@gmail.com>
2020-03-03 15:55:25 +0800

    Update start_v2ray_and_iptables.sh

--- start_v2ray_and_iptables.sh
+++ start_v2ray_and_iptables.sh
@@ -41,7 +41,7 @@ chmod -R +x ${DIR_PATH}
 echo "RUN V2RAY...."
 nohup ${DIR_PATH}/v2ray -config ${FILES_PATH}/${CONFIG_FILE} > /dev/null 2>&1 &
 if [[ $? -ne 0 ]];then
-    echo "run V2RAY failed. now quit...."
+    echo "RUN V2RAY failed. now quit...."
     exit
 fi
 

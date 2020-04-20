2fe5dd58f37e2f0487400a96c3c0c315c8f1c39d
aresbigboy <root@localhost.localdomain>
2020-04-20 11:37:05 +0800

    1587353825

--- start_v2ray_and_iptables.sh
+++ start_v2ray_and_iptables.sh
@@ -20,10 +20,10 @@ fi
 
 if [[ $(iptables-save | grep V2RAY | wc -l) -ne 0 ]];then
     echo "found iptables for V2RAY. now clean rules...."
-    iptables -t nat -D PREROUTING -p tcp -j V2RAY
-    iptables -t nat -D OUTPUT -p tcp -j V2RAY
-    iptables -t nat -F V2RAY
-    iptables -t nat -X V2RAY
+    iptables -w 3 -t nat -D PREROUTING -p tcp -j V2RAY
+    iptables -w 3 -t nat -D OUTPUT -p tcp -j V2RAY
+    iptables -w 3 -t nat -F V2RAY
+    iptables -w 3 -t nat -X V2RAY
 fi
 
 ############################################
@@ -52,22 +52,22 @@ fi
 echo "add iptables for V2RAY rules...."
 
 # 新建一个名为 V2RAY 的链
-iptables -t nat -N V2RAY
+iptables -w 3 -t nat -N V2RAY
 
 # 直连 内网网段
-iptables -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
-iptables -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
-iptables -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
-iptables -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 0.0.0.0/8 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 10.0.0.0/8 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 127.0.0.0/8 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 169.254.0.0/16 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 172.16.0.0/12 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 192.168.0.0/16 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 224.0.0.0/4 -j RETURN
+iptables -w 3 -t nat -A V2RAY -d 240.0.0.0/4 -j RETURN
 
 # 直连 VPS IP
 grep address ${FILES_PATH}/${CONFIG_FILE} | awk -F \" '{print $(NF-1)}' | \
 while read VPSIP;do
-    iptables -t nat -I V2RAY -d ${VPSIP} -j RETURN;
+    iptables -w 3 -t nat -I V2RAY -d ${VPSIP} -j RETURN;
 done
 
 # 直连 国IP段
@@ -75,16 +75,16 @@ echo "adding china ip rules please wait for minitus...."
 sh ${FILES_PATH}/return_ip-cn.sh
 
 # V2ray 发出的流量 直连 (android seam to not support that....)
-#iptables -t nat -A V2RAY -p tcp -m mark --mark 0xff -j RETURN
+#iptables -w 3 -t nat -A V2RAY -p tcp -m mark --mark 0xff -j RETURN
 
 # 其余流量转发到 1099 端口（即 V2Ray）
-iptables -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 1099
+iptables -w 3 -t nat -A V2RAY -p tcp -j REDIRECT --to-ports 1099
 
 # 对局域网其他设备进行透明代理
-iptables -t nat -A PREROUTING -p tcp -j V2RAY
+iptables -w 3 -t nat -A PREROUTING -p tcp -j V2RAY
 
 # 对本机进行透明代理
-iptables -t nat -A OUTPUT -p tcp -j V2RAY
+iptables -w 3 -t nat -A OUTPUT -p tcp -j V2RAY
 #############################################
 
 echo "add iptables for V2RAY finished...."

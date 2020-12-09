ff67dc38b77c64eb3fe4d1e398d6af54c959179c
aresbigboy <root@localhost.localdomain>
2020-12-09 14:56:24 +0800

    change to install Xray-android-arm64-v8a.zip

--- v2ray-linux-arm64/update_latest_Xray-linux-arm64.sh
+++ v2ray-linux-arm64/update_latest_Xray-linux-arm64.sh
@@ -2,7 +2,8 @@
 
 cd $(cd `dirname $0`; pwd)
 
-curl -k -L -H "Cache-Control: no-cache" -o Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip
+#curl -k -L -H "Cache-Control: no-cache" -o Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip
+curl -k -L -H "Cache-Control: no-cache" -o Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-android-arm64-v8a.zip
 
 unzip Xray.zip xray
 chmod +x xray

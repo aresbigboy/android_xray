32ca2c5a8948f5f0ccb10667ef49a2571e9ba651
aresbigboy <root@localhost.localdomain>
2020-12-30 10:00:07 +0800

    add only set iptables rules for userdebug system

--- only_add_iptables/80_443_8080_ports.sh
+++ only_add_iptables/80_443_8080_ports.sh
@@ -1,3 +1,4 @@
 # 对局域网其他设备进行透明代理
-iptables -w 3 -t nat -D PREROUTING -p tcp -m multiport --dports 80,443,8080 -j REDIRECT --to-ports 1099
-iptables -w 3 -t nat -A PREROUTING -p tcp -m multiport --dports 80,443,8080 -j REDIRECT --to-ports 1099
+
+iptables-save | grep "\-\-to\-ports\ 1099$" | grep PREROUTING | sed 's/-A/iptables -t nat -D/g' | sh
+iptables -t nat -A PREROUTING -p tcp -m multiport --dports 80,443,8080 -j REDIRECT --to-ports 1099

ed0f1af4328b46e2f4061fe4b44c5608c1091385
aresbigboy <root@localhost.localdomain>
2021-07-23 11:00:50 +0800

    2021-07-23 11:00:50

--- pause.sh
+++ pause.sh
@@ -1,8 +1,9 @@
 FILES_PATH="$(cd `dirname $0`; pwd)"
 
 if [[ $(iptables-save | grep XRAY | grep -E "OUTPUT|PREROUTING" | wc -l) -ne 0 ]];then
-    echo "found iptables for XRAY. now clean OUTPUT PREROUTING rules...."
+    echo "Clear !!! Found iptables for XRAY. OUTPUT PREROUTING rules has been cleaned ...."
     iptables-save | grep XRAY | grep -E "OUTPUT|PREROUTING" | sed 's/-A/iptables -t nat -D/g' | sh
 else
     grep -E "OUTPUT|PREROUTING" ${FILES_PATH}/start_xray_and_iptables.sh | sh
+    echo "Added !!! Iptables Rules for XRAY. OUTPUT PREROUTING rules has been added ...."
 fi

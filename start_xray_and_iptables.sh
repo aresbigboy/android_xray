d2b1d628809db0646be56e7f53e10213106d21c7
aresbigboy <wei.ares@gmail.com>
2022-03-18 17:14:09 +0800

    Update start_xray_and_iptables.sh

--- start_xray_and_iptables.sh
+++ start_xray_and_iptables.sh
@@ -34,31 +34,12 @@ if [ $? -ne 0 ];then
 fi
 
 
+
 ############################################
 echo "add iptables for XRAY rules...."
 
 # 新建一个名为 XRAY 的链
 iptables -w 3 -t nat -N XRAY
-
-# 直连 内网网段
-iptables -w 3 -t nat -A XRAY -d 0.0.0.0/8 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 10.0.0.0/8 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 127.0.0.0/8 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 169.254.0.0/16 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 172.16.0.0/12 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 192.168.0.0/16 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 224.0.0.0/4 -j RETURN
-iptables -w 3 -t nat -A XRAY -d 240.0.0.0/4 -j RETURN
-
-# 直连 VPS IP
-grep address ${FILES_PATH}/${CONFIG_FILE} | awk -F \" '{print $(NF-1)}' | \
-while read VPSIP;do
-    iptables -w 3 -t nat -I XRAY -d ${VPSIP} -j RETURN;
-done
-
-# 直连 国IP段
-echo "adding china ip rules please wait for minitus...."
-sh ${FILES_PATH}/return_ip-cn.sh
 
 # 其余流量转发到 61099 端口（即 V2Ray）
 iptables -w 3 -t nat -A XRAY -p tcp -j REDIRECT --to-ports 61099
@@ -67,7 +48,9 @@ iptables -w 3 -t nat -A XRAY -p tcp -j REDIRECT --to-ports 61099
 iptables -w 3 -t nat -A PREROUTING -p tcp -m multiport --dports 80,443,8080 -j XRAY
 
 # 对本机进行透明代理
+iptables -w 3 -t nat -I OUTPUT -p tcp -m owner --uid-owner 0 -j RETURN
 iptables -w 3 -t nat -A OUTPUT -p tcp -m multiport --dports 80,443,8080 -j XRAY
+
 #############################################
 
 echo "add iptables for XRAY finished...."

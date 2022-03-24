764df251e64dcdbdfc882e790d087aaf91f72026
aresbigboy <wei.ares@gmail.com>
2022-03-24 15:09:09 +0800

    Add files via upload

--- xray-linux-arm64/update_latest_Xray-linux-arm64.sh
+++ xray-linux-arm64/update_latest_Xray-linux-arm64.sh
@@ -2,9 +2,8 @@
 
 cd $(cd `dirname $0`; pwd)
 
-#curl -k -L -H "Cache-Control: no-cache" -o Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip
-
 curl -k -L -H "Cache-Control: no-cache" -o Xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-android-arm64-v8a.zip $1
 
-unzip Xray.zip xray geosite.dat geoip.dat
+unzip -o Xray.zip xray geo*
+rm -f Xray.zip
 chmod +x xray
